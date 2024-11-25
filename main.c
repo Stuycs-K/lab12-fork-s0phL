32e6e66ea462eb72a50a4514cb45eea1e4fd96bc
somebody <81702427+sOm38Ody@users.noreply.github.com>
2024-11-25 16:57:47 -0500

    continued working on child processes

--- main.c
+++ main.c
@@ -4,28 +4,44 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+void child_process() {
+  unsigned int random_num;
+  int fd = open("/dev/random", O_RDONLY);
+  if (fd == -1) {
+    perror("failed to open dev random");
+    exit(1);
+  }
+  read(fd, &random_num, sizeof(random_num));
+  random_num %= (5 + 1);
+  printf("%d %dsec\n", getpid(), random_num);
+  sleep(random_num);
+  printf("%d finished after %d seconds\n", getpid(), random_num);
+  exit(0);
+}
+
 int main() {
-  printf("%d about to create 2 child processes", getpid());
-  pid_t p = fork();
-  if (p < 0) {
+  printf("%d about to create 2 child processes\n", getpid());
+  pid_t p1 = fork();
+  if (p1 < 0) {
     perror("fork failed");
     exit(1);
   }
-  else if (p == 0) {
-    int random_num;
-    int fd = open("/dev/random", O_RDONLY);
-    if (fd == -1) {
-      perror("failed to open dev random");
-      exit(1);
-    }
-    read(fd, &random_num, sizeof(random_num));
-    printf("%d %dsec", getpid(), random_num);
-    sleep(random_num);
-    printf("%d finished after %d seconds", getpid(), random_num);
-    kill(getpid(), 3);
-
+  else if (p1 == 0) {
+    child_process();
   }
   else {
+    pid_t p2 = fork();
+    if (p1 < 0) {
+      perror("fork failed");
+      exit(1);
+    }
+    else if (p2 == 0) {
+      child_process();
+    }
+    else {
+
+    }
 
   }
+  return 0;
 }
