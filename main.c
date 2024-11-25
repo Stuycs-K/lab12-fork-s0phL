05284c251fe827f5d3acf5c17e1ff809ceee494a
Sophie Liu <sliu50@stuy.edu>
2024-11-25 15:34:00 -0500

    started child process instructions

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+  pid_t p = fork();
+  if (p < 0) {
+    perror("fork failed");
+    exit(1);
+  }
+  else if (p == 0) {
+    int random_num;
+    int fd = open("/dev/random", O_RDONLY);
+    if (fd == -1) {
+      perror("failed to open dev random");
+      exit(1);
+    }
+    read(fd, &random_num, sizeof(random_num));
+    printf("%d %dsec", getpid(), random_num);
+    sleep(random_num);
+    printf("%d finished after %d seconds", getpid(), random_num);
+    kill(getpid(), 3);
+
+  }
+  else {
+
+  }
+}
